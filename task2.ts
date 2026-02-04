4a42ca24064c7433bf1ae3844aaba29a5680b9a3
sdajqgf-max <sdajqgf@gmail.com>
2026-02-04 10:01:39 -0800

    feat:task2 done

--- task2.ts
+++ task2.ts
@@ -1,6 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 // Написать функцию, которая проверяет является ли слово палиндромом
 
 const word1 = "Анна";
 const word2 = "Кабак";
 const word3 = "Кот";
+function checkPalindrome(word: string) {
+  return word.toLowerCase().split("").reverse().join("") === word.toLowerCase();
+}
+console.log(checkPalindrome(word3));
