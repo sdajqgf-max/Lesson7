3a6c446f077ff70814db871947aa26f464f82eea
sdajqgf-max <sdajqgf@gmail.com>
2026-02-04 10:23:55 -0800

    feat: task3 done

--- task3.ts
+++ task3.ts
@@ -1,4 +1,21 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 // Напишите функцию, которая найдет максимальное и минимальное число в массиве
 
 const array: number[] = [5, 2, -4, -8, 23, 55, 34, 0, 8];
+
+function FindeMinMax(arr: number[]) {
+  if (!arr.length) {
+    return " ";
+  }
+  let min = arr[0] as number;
+  let max = arr[0] as number;
+  for (const value of arr) {
+    if (value > max) {
+      max = value;
+    }
+    if (value < min) {
+      min = value;
+    }
+  }
+  return `min value: ${min}; max value: ${max}`;
+}
+console.log(FindeMinMax(array));
